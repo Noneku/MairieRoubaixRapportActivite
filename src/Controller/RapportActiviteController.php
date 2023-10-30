3109c0ab37a4921a0d5f531c3b4e4426bb613e25
Noneku <dev.nassim.pro@gmail.com>
2023-10-30 22:38:57 +0100

    Update crud to entity Rapport

--- src/Controller/RapportActiviteController.php
+++ src/Controller/RapportActiviteController.php
@@ -12,24 +12,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RapportActiviteController extends AbstractController
 {
-    #[Route('/', name: 'app_rapport_activite')]
-    public function index(EntityManagerInterface $entityManager, Request $request): Response
-    {
-        $rapport = new RapportActivite();
-        $form = $this->createForm(RapportActiviteType::class, $rapport);
 
+    #[Route('/', name: 'app_rapport_activite_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $rapportActivite = new RapportActivite();
+        $form = $this->createForm(RapportActiviteType::class, $rapportActivite);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($rapport);
+            $entityManager->persist($rapportActivite);
             $entityManager->flush();
-
-            // return $this->redirectToRoute('page_de_confirmation');
         }
 
         return $this->render('rapport_activite/index.html.twig', [
-            'controller_name' => 'RapportActiviteController',
-            'form' => $form
+            'rapport_activite' => $rapportActivite,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_rapport_activite_show', methods: ['GET'])]
+    public function show(RapportActivite $rapportActivite): Response
+    {
+        return $this->render('rapport_activite/show.html.twig', [
+            'rapport_activite' => $rapportActivite,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_rapport_activite_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, RapportActivite $rapportActivite, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RapportActiviteType::class, $rapportActivite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            // return $this->redirectToRoute('app_rapport_activite_index', [], Response::HTTP_SEE_OTHER);
+
+            return var_dump('test');
+        }
+
+        return $this->render('rapport_activite/edit.html.twig', [
+            'rapport_activite' => $rapportActivite,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/RapportActiviteType.php
+++ src/Form/RapportActiviteType.php
@@ -7,21 +7,35 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RapportActiviteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('missionPrincipale')
-            ->add('indicateur')
-            ->add('realisation')
-            ->add('perspective')
-            ->add('donneesFinance')
-            ->add('donneesRH')
+            ->add('missionPrincipale', TextareaType::class, [
+                'label' => 'Mission Principale'
+            ])
+            ->add('indicateur', TextareaType::class, [
+                'label' => 'Indicateur'
+            ])
+            ->add('realisation', TextareaType::class, [
+                'label' => 'Réalisation'
+            ])
+            ->add('perspective', TextareaType::class, [
+                'label' => 'Perspective'
+            ])
+            ->add('donneesFinance', TextareaType::class, [
+                'label' => 'Données Finances'
+            ])
+            ->add('donneesRH', TextareaType::class, [
+                'label' => 'Données Ressources Humaines'
+            ])
             ->add('indicateurFile', FileType::class, [
-                'required' => false, // Permet de rendre le champ facultatif
-                'label' => 'Fichier pour Indicateur', // Label personnalisé
+                'required' => false,
+                'label' => 'Fichier pour Indicateur'
             ])
             ->add('realisationFile', FileType::class, [
                 'required' => false, // Permet de rendre le champ facultatif
@@ -31,7 +45,6 @@ class RapportActiviteType extends AbstractType
                 'required' => false, // Permet de rendre le champ facultatif
                 'label' => 'Fichier pour Perspective', // Label personnalisé
             ]);
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
