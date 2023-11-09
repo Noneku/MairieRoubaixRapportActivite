0e20100d04aea6095e82970087bdb37e2415ff23
None.ku <dev.nassim.pro@gmail.com>
2023-11-09 22:56:51 +0100

    Feature DL File OK

--- src/Controller/Admin/RapportActiviteCrudController.php
+++ src/Controller/Admin/RapportActiviteCrudController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\IndexPole;
 use App\Entity\RapportActivite;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class RapportActiviteCrudController extends AbstractCrudController
 {
@@ -19,9 +18,10 @@ class RapportActiviteCrudController extends AbstractCrudController
         return RapportActivite::class;
     }
 
-    
+
     public function configureFields(string $pageName): iterable
     {
+
         return [
             // IdField::new('id'),
             // TextField::new('mission_principale'),
@@ -33,10 +33,21 @@ class RapportActiviteCrudController extends AbstractCrudController
             // TextField::new('donneesRH'),
             TextField::new('status', 'Etat'),
             DateTimeField::new('date')
-            ->setFormat('dd-MM-Y HH:mm')
-            ->setFormTypeOption('disabled', true),
-            AssociationField::new('urlIndex')
+                ->setFormat('dd-MM-Y HH:mm')
+                ->setFormTypeOption('disabled', true),
+            AssociationField::new('urlIndex'),
+
+            TextEditorField::new('indicateurFileName', 'Indicateur File')
+                ->setTemplatePath('admin/indicateur_file_widget.html.twig')
+                ->hideOnForm(),
+
+            TextEditorField::new('realisationFileName', 'Realisation File')
+                ->setTemplatePath('admin/realisation_file_widget.html.twig')
+                ->hideOnForm(),
+
+            TextEditorField::new('perspectiveFileName', 'Perspective File')
+                ->setTemplatePath('admin/perspective_file_widget.html.twig')
+                ->hideOnForm()
         ];
     }
-    
 }
